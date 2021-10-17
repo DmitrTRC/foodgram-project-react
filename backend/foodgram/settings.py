83bca0f6abb091690362bc5acfd0ba4b0a148586
afrlv <123Atom123>
2021-10-17 16:40:37 +0300

    Правка settings - добавлены параметры по умолчанию

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,12 +61,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 if os.environ.get("BACKEND_DB_PASS"):
     DATABASES = {
         'default': {
-            'ENGINE': os.environ.get('DB_ENGINE'),
-            'NAME': os.environ.get('DB_NAME'),
+            'ENGINE': os.environ.get('DB_ENGINE', "django.db.backends.postgresql"),
+            'NAME': os.environ.get('DB_NAME', 'localhost'),
             'USER': os.environ.get('POSTGRES_USER'),
             'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
+            'PORT': os.environ.get('DB_PORT', 5432),
         },
     }
 else:
