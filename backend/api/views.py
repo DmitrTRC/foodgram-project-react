6d1651bac43c3bd1df263e6a8ce50c763c76fb1d
afrlv <123Atom123>
2021-10-17 15:16:52 +0300

    Оптимизация удаления рецепта

--- backend/api/views.py
+++ backend/api/views.py
@@ -83,19 +83,14 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def add_obj(self, model, user: users.models.User, pk: int) -> Response:
         if model.objects.filter(user=user, recipe__id=pk).exists():
-            return Response({
-                'errors': 'Рецепт уже добавлен в список'
-            }, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'errors': 'Рецепт уже добавлен в список'}, status=status.HTTP_400_BAD_REQUEST)
         recipe = get_object_or_404(Recipe, id=pk)
         model.objects.create(user=user, recipe=recipe)
         serializer = CropRecipeSerializer(recipe)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def delete_obj(self, model, user: users.models.User, pk: int) -> Response:
-        obj = model.objects.filter(user=user, recipe__id=pk)
+        obj = model.objects.filter(user=user, recipe__id=pk).delete()
         if obj.exists():
-            obj.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response({
-            'errors': 'Рецепт уже удален'
-        }, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'errors': 'Рецепт уже удален'}, status=status.HTTP_400_BAD_REQUEST)
